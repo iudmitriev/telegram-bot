2e00f88bc5f840a66bc8abe7b7c56466bf982f26
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2021-12-19 18:23:57 +0300

    Make code better

--- echo_bot.py
+++ echo_bot.py
@@ -1,20 +1,34 @@
+'''
+Simple echo bot
+'''
+
 import telebot
 
-with open("bot_control_token.txt", "r") as f:
-	token = f.readline().strip()
+with open("bot_control_token.txt", "r", encoding='utf8') as f:
+    token = f.readline().strip()
 
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Welcome!")
+    '''
+    Sends welcome message
+    '''
+    bot.reply_to(message, "Welcome!")
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-	bot.reply_to(message, "This is simple echo bot.")
+    '''
+    Sends description
+    '''
+    bot.reply_to(message, "This is simple echo bot.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    '''
+    Echo message
+    '''
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
